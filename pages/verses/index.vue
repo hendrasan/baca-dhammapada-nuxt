7dd3bbc771b046de2ad8c6fdbfd94765df2ff3c0
Hendra Susanto <anotherhendrasusanto@gmail.com>
2025-03-18 22:59:08 +0700

    feat: add search in verses list page

--- pages/verses/index.vue
+++ pages/verses/index.vue
@@ -4,11 +4,19 @@ import type { Verses } from '~/types/verse';
 const route = useRoute()
 
 const currentPage = ref(Number(route.query.page) || 1)
+const searchQuery = ref(route.query.q as string || '')
+const debouncedSearchQuery = ref(route.query.q as string || '')
 
 const requestParams = computed(() => {
-    return {
+    const params: { page: number, q?: string } = {
         page: currentPage.value,
     };
+
+    if (debouncedSearchQuery.value) {
+        params.q = debouncedSearchQuery.value;
+    }
+
+    return params;
 });
 
 const { data, error } = await useApiFetch<Verses>(`/api/v1/verses`, {
@@ -27,12 +35,20 @@ const meta = data?.value?.meta
 const handlePageChange = (newPage: number) => {
     currentPage.value = newPage
 
-    navigateTo({ query: { page: newPage } })
+    navigateTo({ query: { ...route.query, page: newPage } })
 
     window.scrollTo({
         top: 0,
         behavior: 'smooth'
     });
+}
+
+const handleSearch = () => {
+    currentPage.value = 1;
+
+    debouncedSearchQuery.value = searchQuery.value
+
+    navigateTo({ query: { ...route.query, q: debouncedSearchQuery.value, page: 1 } })
 }
 </script>
 
@@ -51,13 +67,21 @@ const handlePageChange = (newPage: number) => {
         <hr />
 
         <section class="flex flex-col gap-4">
-            <!-- <h2 class="text-center text-3xl text-primary">Daftar Bab</h2> -->
+            <!-- <form class="flex gap-2" @submit.prevent="handleSearch">
+                <input v-model="searchQuery" type="text" placeholder="Cari syair..."
+                    class="px-4 py-2 rounded border border-gray-300 focus:outline-none focus:border-primary" />
+                <Button type="submit">Cari</Button>
+            </form> -->
 
             <div v-if="verses == null">
                 No data
             </div>
 
             <div v-else class="flex flex-col gap-4">
+                <form @submit.prevent="handleSearch" class="flex justify-end items-center gap-4">
+                    <Input class="w-full md:w-[300px]" v-model="searchQuery" placeholder="Cari syair" />
+                </form>
+
                 <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
                     <NuxtLink class="group" v-for="verse in verses" :key="verse.id"
                         :to="{ name: 'verses-id', params: { id: verse.id } }">
@@ -68,16 +92,5 @@ const handlePageChange = (newPage: number) => {
                 <VPagination :meta="meta" :currentPage="currentPage" :handlePageChange="handlePageChange" />
             </div>
         </section>
-
-        <!-- <hr />
-
-        <section class="flex flex-row justify-center gap-4">
-            <Button as-child>
-                <NuxtLink :to="{ name: 'index' }">Lihat Semua Syair</NuxtLink>
-            </Button>
-            <Button as-child>
-                <NuxtLink :to="{ name: 'index' }">Lihat Syair Acak</NuxtLink>
-            </Button>
-        </section> -->
     </div>
 </template>
