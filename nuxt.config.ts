67a40372d91c95f12dc40438c7ecfd21ae961b00
Hendra Susanto <anotherhendrasusanto@gmail.com>
2025-07-11 17:04:16 +0700

    feat: add google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,15 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt", "@nuxtjs/color-mode"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxtjs/color-mode",
+    "nuxt-gtag",
+  ],
+  gtag: {
+    id: "G-H03KF4C82T",
+  },
   colorMode: {
     classSuffix: "",
   },
